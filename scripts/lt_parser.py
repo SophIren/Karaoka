5a10663ddca2bb495870001cefdaa6e0d243699e
SophIren <45230353+SophIren@users.noreply.github.com>
2022-09-25 20:55:02 +0300

    refactoring and using pydub to overlay audio streams

---
+++ scripts/lt_parser.py
@@ -0,0 +1,3 @@
+class LTParser:
+    def __init__(self):
+        pass
--- scripts/recording.py
+++ scripts/recording.py
@@ -1,16 +1,16 @@
 import time
 import pyaudio
-import wave
-import audioop
-import samplerate as sr
+from pydub import AudioSegment
 
 
 class Recorder:
     DEFAULT_FRAMES = 512
+    FRAME_RATE = 48000
+    SAMPLE_WIDTH = 2
+    CHANNEL_COUNT = 2
 
     def __init__(self):
         self.audio = pyaudio.PyAudio()
-        self.resampler = sr.Resampler()
         self.out_rec = None
         self.in_rec = None
         self.out_device = None
@@ -23,10 +23,12 @@ class Recorder:
         self.mic_frames = []
 
         self.in_device = self.audio.get_default_input_device_info()
-        self.in_rec = self.create_stream(self.in_device, False)
+        self.in_rec = self.create_stream(self.in_device['index'], False,
+                                         self.mic_callback)
 
         self.out_device = self.get_default_wasapi_device()
-        self.out_rec = self.create_stream(self.out_device, True)
+        self.out_rec = self.create_stream(self.out_device['index'], True,
+                                          self.speaker_callback)
 
     def stop_recording(self):
         if self.out_rec is not None:
@@ -37,46 +39,34 @@ class Recorder:
             self.in_rec.close()
 
     def save_recorded_frames(self, file_name='output.wav'):
-        channel_count = max(self.in_device['maxInputChannels'],
-                            self.out_device['maxOutputChannels'])
-        sample_rate = max(self.out_device["defaultSampleRate"],
-                          self.in_device["defaultSampleRate"])
-        waveFile = wave.open(file_name, 'wb')
-        waveFile.setnchannels(channel_count)
-        waveFile.setsampwidth(self.audio.get_sample_size(pyaudio.paInt16))
-        waveFile.setframerate(sample_rate)
+        mic_segment = AudioSegment(b''.join(self.mic_frames),
+                                   sample_width=self.SAMPLE_WIDTH,
+                                   frame_rate=self.FRAME_RATE,
+                                   channels=self.CHANNEL_COUNT)
+        speaker_segment = AudioSegment(b''.join(self.speaker_frames),
+                                       sample_width=self.SAMPLE_WIDTH,
+                                       frame_rate=self.FRAME_RATE,
+                                       channels=self.CHANNEL_COUNT)
 
-        self.mic_frames = b''.join(self.mic_frames)
-        self.speaker_frames = b''.join(self.speaker_frames)
-        l = min(len(self.mic_frames), len(self.speaker_frames))
-        waveFile.writeframes(audioop.add(self.mic_frames[:l],
-                                         self.speaker_frames[:l], 2))
-        waveFile.close()
+        combined = mic_segment.overlay(speaker_segment)
+        combined.export(file_name, format='wav')
 
-    def speaker_callback(self, in_data):
+    def speaker_callback(self, in_data, frame_count, time_info, status):
         self.speaker_frames.append(in_data)
         return None, pyaudio.paContinue
 
-    def mic_callback(self, in_data):
+    def mic_callback(self, in_data, frame_count, time_info, status):
         self.mic_frames.append(in_data)
         return None, pyaudio.paContinue
 
-    def create_stream(self, device, as_loopback):
-        if as_loopback:
-            callback = lambda in_data, frame_count, time_info, status: \
-                self.speaker_callback(in_data)
-        else:
-            callback = lambda in_data, frame_count, time_info, status: \
-                self.mic_callback(in_data)
-
+    def create_stream(self, device_index, as_loopback, callback):
         return self.audio.open(
             format=pyaudio.paInt16,
-            channels=device['maxOutputChannels'] if as_loopback
-            else device['maxInputChannels'],
-            rate=48000,
+            channels=self.CHANNEL_COUNT,
+            rate=self.FRAME_RATE,
             input=True,
             frames_per_buffer=self.DEFAULT_FRAMES,
-            input_device_index=device["index"],
+            input_device_index=device_index,
             stream_callback=callback,
             as_loopback=as_loopback)
 
@@ -85,8 +75,8 @@ class Recorder:
 
         for i in range(self.audio.get_device_count()):
             device = self.audio.get_device_info_by_index(i)
-            host = self.audio.get_host_api_info_by_index(
-                device['hostApi'])['name']
+            host = self.audio \
+                .get_host_api_info_by_index(device['hostApi'])['name']
             if device['name'] == def_out['name'] and host == 'Windows WASAPI':
                 return device
 
